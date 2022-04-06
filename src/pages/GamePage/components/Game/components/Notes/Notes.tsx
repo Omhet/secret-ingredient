b1ae1c42ec55e49fd98bda445fcbff3573810a1c
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-04-06 20:52:33 +0300

    move from random angle

--- src/pages/GamePage/components/Game/components/Notes/Notes.tsx
+++ src/pages/GamePage/components/Game/components/Notes/Notes.tsx
@@ -1,8 +1,9 @@
 import { Position } from '@app-types/game';
 import { useGameStore } from '@store/game';
 import { AnimatePresence } from 'framer-motion';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { BEAT_SIZE, HALF_BEAT_SIZE } from '../../constants';
+import { getRandomAngle } from '../../utils';
 import { Note } from './Note/Note';
 
 const LATENCY_COMPENSATION = 0.05;
@@ -14,30 +15,49 @@ type NotesProps = {
 
 export const Notes: FC<NotesProps> = ({ zonePosition, onAnimationComplete }) => {
   const { notes, markup } = useGameStore(({ notes, markup }) => ({ notes, markup }));
+  const [notesCoords] = useState(() => {
+    const map: Record<number, any> = {};
+    notes.forEach((note) => {
+      const angle = getRandomAngle() * (Math.PI / 180);
+      const targetX = HALF_BEAT_SIZE * Math.cos(angle) + zonePosition.x;
+      const targetY = HALF_BEAT_SIZE * Math.sin(angle) + zonePosition.y;
 
-  const targetX = zonePosition.x;
-  const targetY = zonePosition.y - HALF_BEAT_SIZE;
+      const dist = BEAT_SIZE * 5.5;
+      const initX = dist * Math.cos(angle) + targetX;
+      const initY = dist * Math.sin(angle) + targetY;
+
+      map[note] = {
+        initX,
+        initY,
+        targetX,
+        targetY,
+      };
+    });
+
+    return map;
+  });
 
   return (
-    <AnimatePresence>
-      {notes.map((beat) => {
-        const initX = innerWidth / 2 - HALF_BEAT_SIZE / 2;
-        const initY = zonePosition.y - BEAT_SIZE * 5 - HALF_BEAT_SIZE;
+    <>
+      <AnimatePresence>
+        {notes.map((beat) => {
+          const coords = notesCoords[beat];
 
-        return (
-          <Note
-            key={beat}
-            beat={beat}
-            initPos={{ x: initX, y: initY }}
-            targetPos={{ x: targetX, y: targetY }}
-            translateDuration={markup.barDuration + LATENCY_COMPENSATION}
-            translateDelay={markup.spb * beat}
-            beatDuration={markup.spb / 2}
-            size={HALF_BEAT_SIZE}
-            onAnimationComplete={onAnimationComplete}
-          />
-        );
-      })}
-    </AnimatePresence>
+          return (
+            <Note
+              key={beat}
+              beat={beat}
+              initPos={{ x: coords.initX, y: coords.initY }}
+              targetPos={{ x: coords.targetX, y: coords.targetY }}
+              translateDuration={markup.barDuration + LATENCY_COMPENSATION}
+              translateDelay={markup.spb * beat}
+              beatDuration={markup.spb / 2}
+              size={HALF_BEAT_SIZE}
+              onAnimationComplete={onAnimationComplete}
+            />
+          );
+        })}
+      </AnimatePresence>
+    </>
   );
 };
--- src/pages/GamePage/components/Game/utils.ts
+++ src/pages/GamePage/components/Game/utils.ts
@@ -50,3 +50,9 @@ export const checkNoteHit = (note: HTMLDivElement, zonePosition: Position) => {
     return Number(note.dataset.beat);
   }
 };
+
+const ANGLES = [225, 270, 315];
+export const getRandomAngle = () => {
+  const index = Math.floor(Math.random() * ANGLES.length);
+  return ANGLES[index];
+};
