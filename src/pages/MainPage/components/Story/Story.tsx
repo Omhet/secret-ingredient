12321501ca796c530b7b2bfe067b99765eff24df
Daria Artemova <darya.artemova@raiffeisen.ru>
2022-04-10 20:17:02 +0300

    add story page starter

--- src/pages/MainPage/components/Story/Story.tsx
+++ src/pages/MainPage/components/Story/Story.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 import s from './Story.module.scss';
+import classnames from 'classnames';
 
 export type StoryProps = {};
 
 export const Story: FC = () => {
   return (
     <section className={s.storyContainer}>
-      <div className={s.storyItem}>
+      <div className={classnames(s.storyItem, s.paddingLeft)}>
         <span className={s.description}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptas repellat ad voluptate modi
           voluptatem accusantium atque ipsam! Praesentium molestiae culpa dolores obcaecati rerum quia minus at vel enim
@@ -15,6 +16,16 @@ export const Story: FC = () => {
         <div className={s.imgContainer}>
           <img className={s.image} src="pics/story.jpg" />
         </div>
+      </div>
+      <div className={classnames(s.storyItem, s.paddingRight)}>
+        <div className={s.imgContainer}>
+          <img className={s.image} src="pics/story.jpg" />
+        </div>
+        <span className={classnames(s.description, s.alignRight)}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptas repellat ad voluptate modi
+          voluptatem accusantium atque ipsam! Praesentium molestiae culpa dolores obcaecati rerum quia minus at vel enim
+          quam.
+        </span>
       </div>
       <button className={s.nextPageBtn}>Next page</button>
     </section>
