b3ae41e9a313b9ca392a722ca564035f89339dd8
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-04-22 00:13:36 +0300

    add zone

--- src/pages/GamePage/components/Game/components/Scene/PixiApp/pixiGame.ts
+++ src/pages/GamePage/components/Game/components/Scene/PixiApp/pixiGame.ts
@@ -1,15 +1,31 @@
 import { levelDataManager } from '@lib/levels/LevelDataManager';
-import { Application, Sprite } from 'pixi.js';
+import { Application, Container, Sprite } from 'pixi.js';
 
 export const pixiGame = (app: Application) => {
-  const currentLevelData = levelDataManager.getCurrentLevelData();
+  const { images } = levelDataManager.getCurrentLevelData();
 
-  const sprite = Sprite.from(currentLevelData.images.food[0]);
-  app.stage.addChild(sprite);
+  document.addEventListener('click', handleTap);
+  function handleTap() {
+    console.log('tap');
+  }
 
-  let elapsed = 0.0;
-  app.ticker.add((delta) => {
-    elapsed += delta;
-    sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
-  });
+  const unitSize = app.screen.height / 4;
+
+  const zone = new Container();
+  zone.position.x = app.screen.width / 2;
+  zone.position.y = app.screen.height;
+  const zoneInner = Sprite.from('/pics/zone_inner.png');
+  const zoneInnerSize = unitSize;
+  zoneInner.width = zoneInnerSize;
+  zoneInner.height = zoneInnerSize;
+  zoneInner.anchor.set(0.5);
+  const zoneOuter = Sprite.from('/pics/zone_outer.png');
+  const zoneOuterSize = unitSize * 2;
+  zoneOuter.width = zoneOuterSize;
+  zoneOuter.height = zoneOuterSize;
+  zoneOuter.anchor.set(0.5);
+  zone.addChild(zoneOuter);
+  zone.addChild(zoneInner);
+  app.stage.addChild(zone);
+  //   app.ticker.add((delta) => {});
 };
