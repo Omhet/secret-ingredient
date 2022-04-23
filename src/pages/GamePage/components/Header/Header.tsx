63427d32f154e20ec82191098b9bf78d7c0f082e
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-04-23 23:42:08 +0300

    remove redundant header stats

--- src/pages/GamePage/components/Header/Header.tsx
+++ src/pages/GamePage/components/Header/Header.tsx
@@ -4,10 +4,9 @@ import React, { FC, useState } from 'react';
 import s from './Header.module.scss';
 
 export const Header: FC = () => {
-  const { hitCount, blastCount, noteCount } = useGame();
+  const { blastCount } = useGame();
   const [initBlastCount] = useState(blastCount);
-  const { markup, ingredientColors } = levelDataManager.getCurrentLevelData();
-  const levelNoteCount = markup.notes.length;
+  const { ingredientColors } = levelDataManager.getCurrentLevelData();
   const ingredientColor = ingredientColors[0];
 
   const barScale = blastCount / initBlastCount;
@@ -19,13 +18,6 @@ export const Header: FC = () => {
         className={s.blastCountBarInner}
         style={{ backgroundColor: ingredientColor, transform: `scaleX(${barScale})` }}
       ></div>
-      <div className={s.root}>
-        <div>
-          Hits: {hitCount} / {levelNoteCount}
-        </div>
-        <div>Blasts: {blastCount}</div>
-        <div>Notes: {noteCount}</div>
-      </div>
     </>
   );
 };
