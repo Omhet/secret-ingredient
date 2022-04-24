354595534ae68ccd4aaaad37dd203f47e4a864f9
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-04-24 23:08:29 +0300

    fix top level import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
 import reactSvgPlugin from 'vite-plugin-react-svg';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -22,5 +22,8 @@ export default ({ command }) => {
       }),
       tsconfigPaths(),
     ],
+    build: {
+      target: 'esnext',
+    },
   });
 };
