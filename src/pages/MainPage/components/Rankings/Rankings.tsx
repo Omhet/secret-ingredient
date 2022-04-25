58bcc0b5924c81f051c4dacde644ffe5d4f1b6d9
Daria Artemova <darya.artemova@raiffeisen.ru>
2022-04-25 12:11:25 +0300

    add ranking table

--- src/pages/MainPage/components/Rankings/Rankings.tsx
+++ src/pages/MainPage/components/Rankings/Rankings.tsx
@@ -9,19 +9,44 @@ export type RankingItem = {
 };
 
 export const Rankings: FC = () => {
-  const rankings: RankingItem[] = [];
+  const rankings: RankingItem[] = [
+    {
+      user: 'omhetehmo',
+      score: 1234,
+      isCurrentUser: false,
+    },
+    {
+      user: 'a;a;alalalalalalalla',
+      score: 124,
+      isCurrentUser: false,
+    },
+    {
+      user: 'lex',
+      score: 500,
+      isCurrentUser: true,
+    },
+    {
+      user: 'airadavometra',
+      score: 0,
+      isCurrentUser: false,
+    },
+  ];
   return (
     <section id="rankings" className={s.main}>
       <h2 className={s.sectionTitle}>Rankings</h2>
       <div className={s.table}>
+        <span className={s.tableHeader}>Place</span>
         <span className={s.tableHeader}>User name</span>
         <span className={s.tableHeader}>Score</span>
-        {rankings.map((item) => (
-          <>
-            <span className={classnames(s.tableItem, { [s.isCurrentUser]: item.isCurrentUser })}>{item.user}</span>
-            <span className={classnames(s.tableItem, { [s.isCurrentUser]: item.isCurrentUser })}>{item.score}</span>
-          </>
-        ))}
+        {rankings
+          .sort((a, b) => b.score - a.score)
+          .map((item, index) => (
+            <>
+              <span className={classnames(s.tableItem, { [s.isCurrentUser]: item.isCurrentUser })}>{index + 1}</span>
+              <span className={classnames(s.tableItem, { [s.isCurrentUser]: item.isCurrentUser })}>{item.user}</span>
+              <span className={classnames(s.tableItem, { [s.isCurrentUser]: item.isCurrentUser })}>{item.score}</span>
+            </>
+          ))}
       </div>
     </section>
   );
