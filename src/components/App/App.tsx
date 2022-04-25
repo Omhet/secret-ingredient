50dace25f71e19a5aeb09b8d7454f04c3c24bf85
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-04-25 21:20:00 +0300

    update rankings if withUpdate param

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,15 +2,29 @@ import { Modals } from '@components/Modals/Modals';
 import { GamePage } from '@pages/GamePage/GamePage';
 import { MainPage } from '@pages/MainPage/MainPage';
 import { NotFoundPage } from '@pages/NotFoundPage/NotFoundPage';
-import { initUser } from '@store/user';
+import { useLevels } from '@store/levels';
+import { updateUserRankings } from '@store/rankings';
+import { initUser, useUser } from '@store/user';
 import React, { FC, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useSearchParam } from 'react-use';
 import s from './App.module.scss';
 
 export const App: FC = () => {
+  const { globalScore } = useLevels();
+  const { isSignedIn } = useUser();
+  const withRankingsUpdate = Boolean(useSearchParam('withUpdate'));
+
   useEffect(() => {
     initUser();
   }, []);
+
+  useEffect(() => {
+    if (withRankingsUpdate && isSignedIn) {
+      updateUserRankings(globalScore);
+      window.history.pushState({}, '', window.location.origin); // Remove search params
+    }
+  }, [withRankingsUpdate, isSignedIn]);
 
   return (
     <>
