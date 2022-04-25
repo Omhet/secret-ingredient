1bdbfb86ee7b53f2fff9e24262cb7719c61d5310
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-04-25 21:42:35 +0300

    add game resize

--- src/pages/GamePage/components/Game/components/Scene/PixiApp/createPixiApp.ts
+++ src/pages/GamePage/components/Game/components/Scene/PixiApp/createPixiApp.ts
@@ -1,12 +1,25 @@
 import { Application } from 'pixi.js';
 import { pixiGame } from './pixiGame';
 
+const SCALE_FACTOR = 3;
+
 export const createPixiApp = () => {
   const app = new Application({
-    width: innerWidth * 3,
-    height: innerHeight * 3,
+    width: document.documentElement.clientWidth * SCALE_FACTOR,
+    height: document.documentElement.clientHeight * SCALE_FACTOR,
     resolution: 1,
     backgroundAlpha: 0,
+  });
+
+  window.addEventListener('resize', function () {
+    app.renderer.resize(
+      document.documentElement.clientWidth * SCALE_FACTOR,
+      document.documentElement.clientHeight * SCALE_FACTOR
+    );
+  });
+
+  app.view.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
   });
 
   const game = pixiGame(app);
